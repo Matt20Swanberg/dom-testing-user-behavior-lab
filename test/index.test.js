6cdc1b5243cb4667fa58bdd71972ba7c4d87ad64
Matthew Swanberg <matt20swanberg@gmail.com>
2026-04-17 16:47:51 -0400

    Added remaining functions

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+
 const {
     addElementToDOM,
     removeElementFromDOM,
     simulateClick,
     handleFormSubmit
-} = require('./index')
+} = require('../index.js')
 
 // Initializes testing state for a clean start 
 beforeEach(() => {
@@ -17,9 +21,10 @@ beforeEach(() => {
 })
 
 test('addElementToDOM adds content to the correct DOM element', () => {
+    const container = document.getElementById('dynamic-content');
     addElementToDOM('dynamic-content', 'Hello world')
     expect(container).not.toBeNull();
-    
+
     expect(container.innerHTML).toBe('Hello world')
     expect(document.getElementById('error-message').innerHTML).toBe('')
 })
@@ -31,18 +36,43 @@ test('removeElementFromDOM removes an existing element from the DOM', () => {
 })
 
 test('simulateClick updates the DOM with the expected content', () => {
-    simulate 
-    const container = document.getElementById();
-    expect(container).not.toBeNull()
+    const clickComment = 'Hello World'
+    const container = document.getElementById('dynamic-content');
+    expect(container).not.toBeNull();
+    expect(container.innerHTML).toBe('');
+    simulateClick('dynamic-content', clickComment);
+    expect(container.innerHTML).toBe(clickComment);
 
-    const error = document.getElementById('error-message')
-    
 })
 
 test('handleFormSubmit updates the page when the form input contains valid text', () => {
+    const formInput = 'Hello World'
+    const container = document.getElementById('dynamic-content');
+    const errorMessage = document.getElementById('error-message');
+    expect(container).not.toBeNull();
+    expect(errorMessage.innerHTML).toBe('');
+
+    document.getElementById('user-input').value = formInput;
+    handleFormSubmit('user-form', 'dynamic-content');
+    expect(container.innerHTML).toBe(formInput)
+    expect(errorMessage.classList).toContain('hidden')
 
 })
 
 test('handleFormSubmit displays the error message Input cannot be empty when the input is empty', () => {
+    const container = document.getElementById('dynamic-content');
+    const formInput = document.getElementById('user-input');
+    formInput.value = ' ';
+    const errorMessage = document.getElementById('error-message');
 
+    expect(container).not.toBeNull();
+    expect(formInput).not.toBeNull();
+    expect(formInput.value).toBe(' ')
+    expect(errorMessage.innerHTML).toBe('');
+    expect(errorMessage.classList).toContain('hidden');
+
+    handleFormSubmit('user-form', 'dynamic-content');
+    expect(container.innerHTML).toBe('');
+    expect(errorMessage.innerHTML).toContain('Input cannot be empty')
+    expect(errorMessage.classList).not.toContain('hidden')
 })
